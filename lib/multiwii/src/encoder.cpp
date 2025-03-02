01cc949b43a0758e66226ba4414eceaaf053365d
Aaron Spiteri <azzmosphere@gmail.com>
2025-03-02 16:52:28 +1100

    integrating network byte order macros

--- lib/multiwii/src/encoder.cpp
+++ lib/multiwii/src/encoder.cpp
@@ -16,16 +16,16 @@ uint32_t  RrEncoder::decodeUint32(uint8_t* data, int& pos) {
 
 int RrEncoder::splitUint16(uint16_t value, uint8_t* data, int pos) {
     uint16_t encoded = htons(value);
-    data[pos] = (value >> 8) & 0xFF;
-    data[pos + 1] = value & 0xFF;
+    data[pos] = (encoded >> 8) & 0xFF;
+    data[pos + 1] = encoded & 0xFF;
     return pos + 2;
 }
 
 int RrEncoder::splitUint32(uint32_t value, uint8_t *data, int pos) {
     uint32_t encoded = htons(value);
-    data[pos++] = (value >> 24) & 0xFF;
-    data[pos++] = (value >> 16) & 0xFF;
-    data[pos++] = (value >> 8) & 0xFF;
-    data[pos++] = value & 0xFF;
+    data[pos++] = (encoded >> 24) & 0xFF;
+    data[pos++] = (encoded >> 16) & 0xFF;
+    data[pos++] = (encoded >> 8) & 0xFF;
+    data[pos++] = encoded & 0xFF;
     return pos;
 }
--- lib/multiwii/src/rrpheader.hpp
+++ lib/multiwii/src/rrpheader.hpp
@@ -5,10 +5,28 @@
 
 #ifdef NATIVE
 
-#define htons(X) X
-#define htonl(X) X
-#define ntohs(X) X
-#define ntohl(X) X
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+// #define htons(X) X
+// #define htonl(X) X
+// #define ntohs(X) X
+// #define ntohl(X) X
+
+#else
+
+#define htons(X) PP_HTONS(X)
+#define htonl(X) PP_HTONL(X)
+#define ntohs(X) PP_NTOHS(X)
+#define ntohl(X) PP_NTOHL(X)
+
+#define PP_HTONS(x) ((uint16_t)((((x) & (uint16_t)0x00ffU) << 8) | (((x) & (uint16_t)0xff00U) >> 8)))
+#define PP_NTOHS(x) PP_HTONS(x)
+#define PP_HTONL(x) ((((x) & (uint32_t)0x000000ffUL) << 24) | \
+                     (((x) & (uint32_t)0x0000ff00UL) <<  8) | \
+                     (((x) & (uint32_t)0x00ff0000UL) >>  8) | \
+                     (((x) & (uint32_t)0xff000000UL) >> 24))
+#define PP_NTOHL(x) PP_HTONL(x)
 
 #endif
 
