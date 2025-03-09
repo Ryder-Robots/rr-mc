37353623490199ea3ddc4276c1f73ece30ab9f58
Aaron Spiteri <azzmosphere@gmail.com>
2025-03-09 23:42:26 +1100

    added unit tests

--- test/test_server/test_server.cpp
+++ test/test_server/test_server.cpp
@@ -8,18 +8,25 @@ using namespace fakeit;
 
 void test_serial_read() {
    
-    When(Method(ArduinoFake(Serial), read)).Return('$');
-    When(Method(ArduinoFake(Serial), read)).Return('M');
-    When(Method(ArduinoFake(Serial), read)).Return(0);
-    When(Method(ArduinoFake(Serial), read)).Return(0);
-    When(Method(ArduinoFake(Serial), read)).Return(static_cast<uint8_t>(RrCommand::MSP_STATUS));
-    When(Method(ArduinoFake(Serial), read)).Return(0);
-    When(Method(ArduinoFake(Serial), read)).Return(0x1E);
+    When(Method(ArduinoFake(Serial), read)).Return('$', 'M', 0, 0, static_cast<uint8_t>(RrCommand::MSP_STATUS), 0, 0x1E);
+    When(Method(ArduinoFake(Serial), available)).Return(1);
+    //When(Method(ArduinoFake(Serial), begin)).AlwaysReturn();
     
     SerialUsb serialUsb = SerialUsb();
     uint8_t* data = serialUsb.read();
 
     TEST_ASSERT_EQUAL_UINT8('$', data[0]);
+    TEST_ASSERT_EQUAL_UINT8('M', data[1]);
+    TEST_ASSERT_EQUAL_UINT8(0, data[2]);
+    TEST_ASSERT_EQUAL_UINT8(0, data[3]);
+    TEST_ASSERT_EQUAL(static_cast<uint8_t>(RrCommand::MSP_STATUS), data[4]);
+    TEST_ASSERT_EQUAL_UINT8(0, data[5]);
+    TEST_ASSERT_EQUAL_UINT8( 0x1E, data[6]);
+
+    serialUsb.available();
+    serialUsb.begin(9600);
+    Verify(Method(ArduinoFake(Serial), available)).Once();
+    //Verify(Method(ArduinoFake(Serial), begin).Using(9600)).Once();
 }
 
 void setUp(void) {
